c73425a7a89fa335acd67d778d3f3f532629fb30
Johan Lübcke <johan@lubcke.se>
2024-07-01 21:37:45 +0200

    Double tap reset

--- keyboards/splitkb/kyria/rev3/keymaps/jlubcke/config.h
+++ keyboards/splitkb/kyria/rev3/keymaps/jlubcke/config.h
@@ -102,3 +102,8 @@
 //# ifdef CONWAY_ENABLE
 //#define SPLIT_TRANSACTION_IDS_USER CONWAY_SYNC
 //# endif
+//
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+// #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
