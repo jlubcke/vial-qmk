3ef27f8a4d90e224a307a8176171b9b021429809
Johan Lübcke <johan@lubcke.se>
2025-05-13 22:07:56 +0200

    Fix anim speed and status display

--- keyboards/splitkb/kyria/rev3/keymaps/jlubcke/keymap.c
+++ keyboards/splitkb/kyria/rev3/keymaps/jlubcke/keymap.c
@@ -173,56 +173,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation)
 	return OLED_ROTATION_180;
 }
 
-static void render_current_layer(void)
-{
-  // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-    switch (get_highest_layer(layer_state|default_layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("Vanilla\n"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("Lower\n"), false);
-            break;
-        case _UPPER:
-            oled_write_P(PSTR("Upper\n"), false);
-            break;
-        case _ADJUST:
-            oled_write_P(PSTR("Adjust\n"), false);
-            break;
-        default:
-            oled_write_P(PSTR("WUT?\n"), false);
-    }
-
-    // Write host Keyboard LED Status to OLEDs
-    led_t led_usb_state = host_keyboard_led_state();
-    oled_write_P(led_usb_state.num_lock    ? PSTR("NUMLCK ") : PSTR("       "), false);
-    oled_write_P(led_usb_state.caps_lock   ? PSTR("CAPLCK ") : PSTR("       "), false);
-    oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
-}
-
-/**
- ** return: if true, start scrolling, to reduce burn in
- ** return: if false, content changed, do not scroll
- */
-static bool render_status(void)
-{
-    static uint8_t scroll_timer = 0;
-
-    render_current_layer();
-    // Host Keyboard LED Status
-    //uint8_t led_usb_state = host_keyboard_leds();
-    //oled_write_P() is_keyboard_master()
-
-    if (scroll_timer < 255)
-        scroll_timer++;
-
-    if (scroll_timer < 255)
-        return false;
-    else
-        return true;
-}
-
 static int c_frame = 0;
 bool first_render = true;
 
@@ -240,14 +190,41 @@ bool oled_task_user(void)
 {
     if (is_keyboard_master())
     {
-        // Will scroll OLED, if "home" screen was displayed for 255 refreshes
-        // TODO: random vertical offset
-        oled_set_cursor(0,4);
-        bool render_stat_ret = render_status(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
-        if (render_stat_ret)
-            oled_scroll_left();
-        else
-            oled_scroll_off();
+         // QMK Logo and version information
+        // clang-format off
+        static const char PROGMEM qmk_logo[] = {
+            0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+            0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+            0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+        // clang-format on
+
+        oled_write_P(qmk_logo, false);
+        oled_write_P(PSTR("\nDon't panic!\n\n"), false);
+
+        // Host Keyboard Layer Status
+        oled_write_P(PSTR("Layer: "), false);
+        switch (get_highest_layer(layer_state|default_layer_state)) {
+            case _QWERTY:
+                oled_write_P(PSTR("Vanilla\n"), false);
+                break;
+            case _LOWER:
+                oled_write_P(PSTR("Lower\n"), false);
+                break;
+            case _UPPER:
+                oled_write_P(PSTR("Upper\n"), false);
+                break;
+            case _ADJUST:
+                oled_write_P(PSTR("Adjust\n"), false);
+                break;
+            default:
+                oled_write_P(PSTR("WUT?\n"), false);
+        }
+
+        // Write host Keyboard LED Status to OLEDs
+        led_t led_usb_state = host_keyboard_led_state();
+        oled_write_P(led_usb_state.num_lock    ? PSTR("NUMLCK ") : PSTR("       "), false);
+        oled_write_P(led_usb_state.caps_lock   ? PSTR("CAPLCK ") : PSTR("       "), false);
+        oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
     } else
     {
         oled_on();
