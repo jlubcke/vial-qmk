02bbd1bbe6d829af3b91628db5f2e9e49bbc2b8f
Johan Lübcke <johan@lubcke.se>
2025-05-21 20:10:15 +0200

    Move files up in tree

---
+++ keyboards/splitkb/kyria/keymaps/jlubcke/rules.mk
@@ -0,0 +1,18 @@
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+ENCODER_MAP_ENABLE = yes
+
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
+
+WPM_ENABLE = yes
+EXTRAKEY_ENABLE = yes  # Media
+
+QMK_LOGO_ENABLE = no
+KYRIA_LOGO_ENABLE = no
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+CONVERT_TO = liatris
